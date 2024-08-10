eb953cbdc83eb470c32339a424cc86ae955296df
Madeline Conway <conway.maddy@yahoo.com>
2024-08-10 19:11:33 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement ("H1")
+//set an id of "victory"
+newHeader.setAttribute('id','victory')
+newHeader.textContent = 'Maddy is the champion'
